4cdfadb5711530c95eb7569560fb364b46fc84ad
Sonjongkook <63791870+Sonjongkook@users.noreply.github.com>
2020-06-30 11:23:43 -0500

    video change
    changes in video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -14,7 +14,7 @@ export const home = async(req, res) => {
 };
 
 export const search = async(req, res) => {
-    const {query: {term:searchingBy}} = req;
+    const {query: {term:searchingBy}} = req;  // const searchingBy = req.query.term 과 같음
     let videos = [];
     try{
         videos = await Video.find({title: {$regex: searchingBy, $options: "i"}})
@@ -22,7 +22,7 @@ export const search = async(req, res) => {
     }catch(error){
         console.log(error);
     }
-    res.render("search", { pageTitle:"Search",searchingBy,videos});
+    res.render("search", { pageTitle:"Search",searchingBy,videos}); //searchingBy = searchingBy
 }
 export const getUpload = (req, res) => res.render("upload", { pageTitle:"Upload"});
 export const PostUpload = async(req, res) => {
